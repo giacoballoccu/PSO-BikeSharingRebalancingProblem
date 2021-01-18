61ec918a99c4c55447873701a097ee681f9a5abb
giacoballoccu <giacoballoccu@live.it>
2021-01-18 11:14:38 +0100

    Creation of a utils file for the PSO

---
+++ PSO/PSOutils.cpp
@@ -0,0 +1,33 @@
+//
+// Created by Giacomo Balloccu on 18/01/2021.
+//
+
+#include <string>
+#include "PSOutils.h"
+namespace PSOutils{
+    /*Convert vector of double into a string useful to cout the results*/
+    string PSOutils::doubleVectorToString(vector<double> vec) {
+        string ans;
+        for(double x : vec){
+            ans += to_string(x);
+            ans += ' ';
+        }
+        return ans;
+    }
+    // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+    void PSOutils::KnuthShuffle(vector<int>& arr){
+        for (int i = arr.size() - 1; i > 0; i--){
+            int index = rand() % i+1;
+            swap(arr[index], i);
+        }
+    }
+    /*Convert an input vector of int in a vector of double */
+    vector<double> PSOutils::copyVecIntToVecDouble(const vector<int> vec) {
+        vector<double> ans;
+        for(int val : vec) {
+            ans.push_back(double(val));
+        }
+        return ans;
+    }
+
+}
---
+++ PSO/PSOutils.h
@@ -0,0 +1,19 @@
+//
+// Created by Giacomo Balloccu on 18/01/2021.
+//
+
+#ifndef BIKEREDISTRIBUTIONHEURISTIC_PSOUTILS_H
+#define BIKEREDISTRIBUTIONHEURISTIC_PSOUTILS_H
+
+#include "stdio.h"
+#include "vector"
+using namespace std;
+
+namespace PSOutils {
+    string doubleVectorToString(vector<double> v);
+    void KnuthShuffle(vector<int>& arr);
+    vector<double> copyVecIntToVecDouble(const vector<int> vec);
+};
+
+
+#endif //BIKEREDISTRIBUTIONHEURISTIC_PSOUTILS_H
