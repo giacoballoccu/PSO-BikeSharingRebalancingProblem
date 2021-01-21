070ab6235aed5c72705c9bc413fe529ab30fa210
giacoballoccu <giacoballoccu@live.it>
2021-01-21 17:56:45 +0100

    Improved visibily of the graph and code readibility

--- GraphVisualizer/GraphPlot.py
+++ GraphVisualizer/GraphPlot.py
@@ -2,54 +2,65 @@ from array import array
 
 import networkx as nx
 import matplotlib.pyplot as plt
-
-def main():
+def read_nodes():
     with open('.nodes.txt') as f:
         lines = f.readlines()
-
         nodes = {}
-        pos1 = {}
         for idx, line in enumerate(lines):
             node_infos = line.split()
             demand = node_infos[0]
             coords = (float(node_infos[1])/100, float(node_infos[2])/100)
-            pos1[str(idx)] = [float(node_infos[1])/100, float(node_infos[2])/100]
-            nodes[idx] = [demand, coords]
+            nodes[str(idx)] = [demand, coords]
 
     f.close()
 
+    print(nodes)
+    return nodes
+def read_edges():
+    optimal_route_str = ""
     with open('.edges.txt') as f:
-        line = f.readline().split()
+        line = f.readlines()[-1].split()
         edges = []
-        for i in range(0,len(line)-1):
+        for i in range(0,len(line)-1, 2):
             edges.append((line[i], line[i+1]))
-
+            optimal_route_str = optimal_route_str + "[" + line[i] + " -> " + line[i+1] + "]" + ", "
     f.close()
 
+    print(edges)
+    return edges, optimal_route_str
+def main():
+    #Read nodes from file
+    nodes = read_nodes()
+    nNodes = len(nodes)
 
-    G = nx.DiGraph()
-    for idx, edge in enumerate(edges):
-        G.add_edges_from([edge], weight=idx)
+    #Read edges and costruct string with optimal route
+    edges, optimal_route_str = read_edges()
 
+    #Create multigraph for plotting
+    G = nx.MultiDiGraph()
+    G.add_edges_from(edges)
 
+    #Set nodes color, red for depots and blue for normal stations
+    color_map = ['darkred' if k == '0' or k == str(nNodes-1) else 'blue' for k in nodes.keys()]
+    #Set edge color, green for main path, black for fullfill from depot
+    edge_colours = ['grey' if edge[1] == '0' or edge[0] == '0' else 'black' for edge in G.edges()]
+    edge_colours[0] = 'black'
 
-    #values = [val_map.get(node, 0.25) for node in G.nodes()]
-
-    # Specify the edges you want here
-    # red_edges = [('A', 'C'), ('E', 'C')]
-    # edge_colours = ['black' if not edge in red_edges else 'red'
-                    #for edge in G.edges()]
-    # black_edges = [edge for edge in G.edges() if edge not in red_edges]
-
-    # Need to create a layout when doing
-    # separate calls to draw nodes and edges
+    #Set position of nodes according to real positions and set label with demand
     pos = nx.spring_layout(G)
-    for k,v in pos1.items():
-        pos[k] = v
-    nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'),
+    for k,v in nodes.items():
+        pos[k] = v[1]
+        #Node label with demand amount
+        if(k != str(0) and k != str(nNodes-1)):
+            plt.text(pos[k][0],pos[k][1]-0.05,s='Demand =' + nodes[str(k)][0], bbox=dict(facecolor='blue', alpha=0.5),horizontalalignment='center')
+
+    #Drawing the graph
+    plt.title("Optimal path: " + optimal_route_str)
+    nx.draw_networkx_nodes(G, pos, node_color=color_map,
                             node_size = 500)
-    nx.draw_networkx_labels(G, pos)
-    nx.draw_networkx_edges(G, pos, edgelist=G.edges, edge_color='r', arrows=True)
+    nx.draw_networkx_labels(G, pos, font_color='white')
+    nx.draw_networkx_edges(G, pos, edgelist=G.edges, edge_color=edge_colours, arrows=True, width=1.5, connectionstyle='arc3, rad = 0.2')
+    #nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
     plt.show()
 
 if __name__ == "__main__":
